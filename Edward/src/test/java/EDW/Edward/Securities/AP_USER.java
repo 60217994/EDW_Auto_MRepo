a264a90b5c8cee36aabc3c511bdf10d1fc205082
60217994 <90447209+60217994@users.noreply.github.com>
2022-04-01 13:14:02 +1100

    negative tests added

--- Edward/src/test/java/EDW/Edward/Securities/AP_USER.java
+++ Edward/src/test/java/EDW/Edward/Securities/AP_USER.java
@@ -23,13 +23,31 @@ public class AP_USER {
 	@Test
 	public void jobStatus() throws SQLException, Exception
 	{
-		List<String> aptables = Arrays.asList("FACT_ED_SE","FACT_AP_SE", "FACT_AP_SE_KPI", "FACT_AP_SE_NOTIF_CANCER", "FACT_AP_SE_SEG", "FACT_AP_SE_WAU", "FACT_AP_SVC_ENC", "v_FACT_AP_SE", "v_FACT_AP_SE", "v_FACT_AP_SE_DIAGNOSIS", "v_FACT_AP_SE_INTERVENTION", "v_FACT_AP_SE_NOTIF_CA", "v_FACT_AP_SE_NOTIF_CANCER", "v_FACT_AP_SE_NOTIF_CANCER", "v_FACT_AP_SE_SEG", "v_FACT_AP_SE_SEG", "v_FACT_AP_SE_WAU", "v_FACT_AP_SE_WAU", "v_FACT_AP_SERVICE_ENC", "v_FACT_AP_SRV_ENC", "v_FACTF_AP_SE_DIAG", "v_FACTF_AP_SE_INTERVENTION", "v_FACTF_AP_SE_WAU", "v_FACTF_AP_SE_WAU_old");
+		// Ap tables list
+		List<String> aptables = Arrays.asList("FACT_AP_SE", "FACT_AP_SE_KPI", "FACT_AP_SE_NOTIF_CANCER", "FACT_AP_SE_SEG", "FACT_AP_SE_WAU", "FACT_AP_SVC_ENC", "v_FACT_AP_SE", "v_FACT_AP_SE", "v_FACT_AP_SE_DIAGNOSIS", "v_FACT_AP_SE_INTERVENTION", "v_FACT_AP_SE_NOTIF_CA", "v_FACT_AP_SE_NOTIF_CANCER", "v_FACT_AP_SE_NOTIF_CANCER", "v_FACT_AP_SE_SEG", "v_FACT_AP_SE_SEG", "v_FACT_AP_SE_WAU", "v_FACT_AP_SE_WAU", "v_FACT_AP_SERVICE_ENC", "v_FACT_AP_SRV_ENC", "v_FACTF_AP_SE_DIAG", "v_FACTF_AP_SE_INTERVENTION", "v_FACTF_AP_SE_WAU", "v_FACTF_AP_SE_WAU_old");
+		//Negative test list
+		List<String> nonaptables = Arrays.asList("COMBINED_CENSUS", "COMBINED_CENSUS_YTD", "EDWARD_CENSUS_EXTRACT", "EDWARD_CENSUS_EXTRACT_YTD");
+		
 		for(int i=0; i < aptables.size() ; i++)
 		{
 			try 
 			{
 				int counts = bc.countsForATable("LRS_MOH.CERTIFIED." + aptables.get(i));
 				System.out.println(aptables.get(i)+" : " + counts);
+			}
+			catch (SQLException e)
+			{
+				System.out.println(e.getMessage());
+			}
+		
+		}
+		
+		for(int i=0; i < nonaptables.size() ; i++)
+		{
+			try 
+			{
+				int counts = bc.countsForATable("LRS_MOH.CERTIFIED." + nonaptables.get(i));
+				System.out.println(nonaptables.get(i)+" : " + counts);
 			}
 			catch (SQLException e)
 			{
